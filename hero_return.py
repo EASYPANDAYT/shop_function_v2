e40fa04ddb612dc4cdddaa346214310c5e33651c
EASYPANDAYT <145465554+EASYPANDAYT@users.noreply.github.com>
2023-12-22 17:44:47 +0300

    Add files via upload

--- hero_return.py
+++ hero_return.py
@@ -11,8 +11,7 @@ def get_hero(
         ) -> list:
     ''' Возвращает список - героя с 6 характеристиками '''
     if not name:
-        names = ('Роман', 'Валера', 'Акакий', 'Мао')
-        name = choice(names)
+        name = choice(['Роман', 'Валера', 'Акакий', 'Мао'])
     if not inventory:
         inventory = []
     return [name, hp, level, xp, money, inventory]
@@ -26,6 +25,7 @@ def show_hero(hero: list) -> None:
     print('опыт:', hero[3])
     print('деньги:', hero[4])
     print('инвентарь:', *hero[5])
+    print('-' * 10)
 
 
 def visit_shop(hero: list, shop_items: list):
@@ -38,6 +38,8 @@ def visit_shop(hero: list, shop_items: list):
     '''
     show_hero(hero)
     print(f'{hero[0]}, приехал в лавку')
+    print('Временная распродажа: все товары по 10 монет!')
+    price_tmp = 10
     print('1 - Купить предметы')
     print('2 - Продать предметы')
     print('0 - Выход из лавки')
@@ -45,10 +47,27 @@ def visit_shop(hero: list, shop_items: list):
     if option == '1':
         for num, item in enumerate(shop_items, 1):
             print(f'{num} - {item}')
-        option = input('Введите номер товара чтобы купить или введите 0 для отмены: ')
-        # TODO: купить товар по выбранной опции
+        print('0 - Отмена')
+        option = input('Введите номер опции чтобы купить или 0 для отмены: ')
+        if int(option) < 0 or int(option) > len(shop_items):
+            print('Неверная опция')
+        elif option == '0':
+            print('Выходим из покупок')
+        else:
+            item_index = int(option) - 1
+            item_name = shop_items[item_index]
+            #print(f'{hero[0]} купил {item_name}')
+            if hero[4] < price_tmp:
+                print(f'У {hero[0]} недостаточно денег')
+            else:
+                hero[4] -= price_tmp
+                hero[5].append(item_name)
+                shop_items.pop(item_index)
+                print(f'{hero[0]} купил {item_name}')
 
 
 player = get_hero()
 shop_items = ['зелье лечения', 'зелье лечения', 'зелье копчения']
 visit_shop(player, shop_items)
+show_hero(player)
+print(shop_items)
